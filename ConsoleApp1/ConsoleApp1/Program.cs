8e642e51511131e1fd5c98cecd4cf9c9f7f47f76
armanixi <36953037+armanixi@users.noreply.github.com>
2018-04-26 00:57:48 +0800

    111

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // 將所有號碼加入 List 中
+            List<int> members = new List<int>();
+
+            for (int i = 1; i <= 42; i++)
+            {
+                members.Add(i);
+            }
+
+            // 隨機挑出一個印出
+            Random rand = new Random();
+
+            for (int i = 0; i < 6; i++)
+            {
+                int r = rand.Next(1, members.Count);
+                Console.Write(members[r] + " ");
+
+                // 並從 List 中刪除
+                members.RemoveAt(r);
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
